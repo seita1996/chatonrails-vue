5062e4a51b845a8f4e9b5e9da9c112587114e57f
seita1996 <taha.seita@gmail.com>
2022-02-13 15:12:32 +0900

    add tailwindcss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_13_032313) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_13_054138) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
