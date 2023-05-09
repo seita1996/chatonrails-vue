76a2690327e74b14bf52cd01b2ddd328984c44c5
seita1996 <taha.seita@gmail.com>
2023-05-10 00:14:49 +0900

    validation without data persistence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,31 @@
-class User < ApplicationRecord
-  def is_test?
-    self.name.include?('test')
+class User
+  include ActiveModel::Model
+  include ActiveModel::Validations::Callbacks
+
+  attr_accessor :name, :email
+
+  validates :name, presence: true
+  validates :email, presence: true
+
+  before_validation :before_validation
+  after_validation :after_validation
+
+  def save
+    if valid?
+      # ここにvalidation成功後の処理を書く
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def before_validation
+    puts 'before_validation'
+  end
+
+  def after_validation
+    puts 'after_validation'
   end
 end
